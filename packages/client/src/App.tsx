b54127371ad22809085d3415f34276d137356bf4
Jack Koppa <hello@jackkoppa.com>
2025-05-31 15:48:17 -0400

    chore: Upgrade to React 19

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,13 @@
 import { Routes, Route, Navigate } from 'react-router'
 import HomePage from './routes/HomePage'
 import EpisodeRoute from './routes/EpisodeRoute'
-import { log } from './utils/logging';
 import { useTheme } from './hooks/useTheme'
 
 /**
  * Main App component that sets up routing configuration.
  */
 function App() {
-  const { theme } = useTheme()
+  useTheme()
 
   return (
     <Routes>
