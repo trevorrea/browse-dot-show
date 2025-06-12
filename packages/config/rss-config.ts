ed06b1854d7ed680a45cd018118e40fc59f7107e
Jack Koppa <hello@jackkoppa.com>
2025-06-12 17:57:24 -0400

    chore: Switch back to football-cliches config

--- packages/config/rss-config.ts
+++ packages/config/rss-config.ts
@@ -1,9 +1,16 @@
 export const RSS_CONFIG = {
-    'claret-and-blue': {
-        id: 'claret-and-blue',
-        rssFeedFile: 'claret-and-blue.xml',
-        title: 'Claret & Blue',
+    'football-cliches': {
+        id: 'football-cliches',
+        rssFeedFile: 'football-cliches.xml',
+        title: 'Football Cliches',
         status: 'active',
-        url: 'https://feeds.megaphone.fm/RPSL1112526468',
+        url: 'https://feeds.megaphone.fm/GLT7974369854',
     },
+    'for-our-sins-the-cliches-pod-archive': {
+        id: 'for-our-sins-the-cliches-pod-archive',
+        rssFeedFile: 'for-our-sins-the-cliches-pod-archive.xml',
+        title: 'For Our Sins: The Clichés Pod Archive',
+        status: 'active',
+        url: 'https://feeds.megaphone.fm/tamc1411507018',
+    }
 } as const;
