9ac243a4a30bf1455865cb23f2c451fb06354df3
Jack Koppa <hello@jackkoppa.com>
2025-07-16 08:13:22 -0400

    fix: Update ThemeToggle style

--- packages/homepage/src/components/ThemeToggle.tsx
+++ packages/homepage/src/components/ThemeToggle.tsx
@@ -17,14 +17,14 @@ export function ThemeToggle() {
   // https://github.com/jnsahaj/tweakcn/blob/3ede03913eb68b65d264716c448dea50f8408dfb/components/editor/action-bar/components/theme-toggle.tsx#L19
   return (
     <div className="px-2">
-      <Tooltip>
+      <Tooltip defaultOpen={false}>
         <TooltipTrigger>
           <SwitchPrimitive.Root
             checked={theme === "dark"}
             onClick={handleThemeToggle}
             className="peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-accent data-[state=unchecked]:bg-input"
           >
-            <SwitchPrimitive.Thumb className="pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0 flex items-center justify-center">
+            <SwitchPrimitive.Thumb className="pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0 flex items-center justify-center text-foreground">
               {theme === "dark" ? (
                 <Moon className="size-3" />
               ) : (
